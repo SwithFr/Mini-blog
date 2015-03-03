1c8215a61424a6755390faec26f6e61109e40e8b
Jeremy Smith <contact@swith.fr>
2015-03-03 11:57:54 +0100

    add disconnect function

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -2,20 +2,19 @@
 
 class UsersController extends AppController
 {
-    public function login()
+    public function check()
     {
-        if (empty($_REQUEST['email']) || empty($_REQUEST['password']))
-        {
-            die('Pas bon :p');
+        if($this->request->method === "POST") {
+            if (empty($_REQUEST['email']) || empty($_REQUEST['password']))
+                die('Pas bon :p');
+            $user = $this->User->get($_REQUEST['email'],sha1($_REQUEST['password']));
+            if ($user) {
+                $this->connect($user->email);
+            } else {
+                $this->create($_REQUEST['email'],sha1($_REQUEST['password']));
+            }
         }
 
-        $user = $this->User->get($_REQUEST['email'],sha1($_REQUEST['password']));
-
-        if ($user) {
-            $this->connect($user->email);
-        } else {
-            $this->create($_REQUEST['email'],sha1($_REQUEST['password']));
-        }
     }
 
     private function connect($email)
@@ -31,5 +30,12 @@ class UsersController extends AppController
         $this->connect($email);
     }
 
+    public function disconnect()
+    {
+        session_destroy();
+        unset($_SESSION ['user']);
+        unset($_SESSION ['connected']);
+        header('Location: http://localhost');
+    }
 
 } 
