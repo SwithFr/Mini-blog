7ac807cbf418966ef11f0cfd4087d31b05f596c6
Jeremy Smith <contact@swith.fr>
2015-03-03 11:29:11 +0100

    add get and create methode for model user

--- models/User.php
+++ models/User.php
@@ -2,5 +2,19 @@
 
 class User
 {
+    public function get($email,$password)
+    {
+        $sql = "SELECT * FROM users WHERE email=:email AND password=:password";
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':email'=>$email,':password'=>$password]);
+        return $pdost->fetch();
+    }
+
+    public function create($email, $password)
+    {
+        $sql = "INSERT INTO users(email,password) VALUES(:email,:password)";
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':email'=>$email,':password'=>$password]);
+    }
 
 }
