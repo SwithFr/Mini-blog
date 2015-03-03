6b1d06274d687d61d8aa6a30f2bf1d5dd1ccfc8e
Jeremy Smith <contact@swith.fr>
2015-03-03 13:05:37 +0100

    add link to connect

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -10,6 +10,12 @@
 <header class="blog__header">
     <h1 class="blog__title">Le blog des patates frâiches !</h1>
 </header>
+    <?php if(!isset($_SESSION['connected']) && !isset($_COOKIE['email'])): ?>
+        <a href="<?= $_SERVER['PHP_SELF']; ?>?a=check&e=user">S'identifier</a>
+    <?php else: ?>
+        <a href="<?= $_SERVER['PHP_SELF']; ?>?a=disconnect&e=user">Se deconnecter</a>
+    <?php endif; ?>
+
     <?php include('./views/' . $controller->view); ?>
 <footer class="blog__footer">
     <p class="blog__footer__content">Tous droits réservés aux kiwis rouge - 2015 | <a class="linkTo" href="<?= $_SERVER['PHP_SELF']; ?>?a=admin_index&e=post">Administration</a></p>
