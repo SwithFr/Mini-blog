1e9c7b25b18a7c2fdb160718b55ee33d16686e0b
Jeremy Smith <contact@swith.fr>
2015-03-03 11:46:49 +0100

    rename login function to check add function connect

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -2,5 +2,26 @@
 
 class UsersController extends AppController
 {
+    public function login()
+    {
+        if (empty($_REQUEST['email']) || empty($_REQUEST['password']))
+        {
+            die('Pas bon :p');
+        }
 
+        $user = $this->User->get($_REQUEST['email'],sha1($_REQUEST['password']));
+
+        if ($user) {
+            $this->connect($user);
+        } else {
+            $this->create($_REQUEST['email'],sha1($_REQUEST['password']));
+        }
+    }
+
+    public function connect($user)
+    {
+        $_SESSION['user'] = $user->email;
+        $_SESSION['connected'] = 1;
+        header("Location: " . $_SERVER['PHP_SELF']);
+    }
 } 
