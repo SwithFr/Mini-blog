ff6e1b920bafb80194f45d4e3d1850b500574085
Jeremy Smith <contact@swith.fr>
2015-03-03 13:06:36 +0100

    modif

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -4,14 +4,14 @@ class UsersController extends AppController
 {
     public function check()
     {
-        if($this->request->method === "POST") {
+        if ($this->request->method === "POST") {
             if (empty($_REQUEST['email']) || empty($_REQUEST['password']))
                 die('Pas bon :p');
-            $user = $this->User->get($_REQUEST['email'],sha1($_REQUEST['password']));
+            $user = $this->User->get($_REQUEST['email'], sha1($_REQUEST['password']));
             if ($user) {
                 $this->connect($user->email);
             } else {
-                $this->create($_REQUEST['email'],sha1($_REQUEST['password']));
+                $this->create($_REQUEST['email'], sha1($_REQUEST['password']));
             }
         }
 
@@ -21,21 +21,23 @@ class UsersController extends AppController
     {
         $_SESSION['user'] = $email;
         $_SESSION['connected'] = 1;
+        setcookie("user", $email, time() + 15 * 24 * 3600);
         header("Location: " . $_SERVER['PHP_SELF']);
     }
 
-    private function create($email,$password)
+    private function create($email, $password)
     {
-        $this->User->create($email,$password);
+        $this->User->create($email, $password);
         $this->connect($email);
     }
 
     public function disconnect()
     {
         session_destroy();
-        unset($_SESSION ['user']);
-        unset($_SESSION ['connected']);
-        header('Location: http://localhost');
+        unset($_SESSION['user']);
+        unset($_SESSION['connected']);
+        setcookie("user", "", time() - 15 * 24 * 3600);
+        header('Location: ' . $_SERVER['PHP_SELF']);
     }
 
 } 
